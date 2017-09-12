6f781ebb4b3dcbfcc743a81d10e579982a851726
speckospock <ben.hq.stephens@gmail.com>
2017-09-11 19:05:12 -0700

    Implements click functionality

--- src/components/app.js
+++ src/components/app.js
@@ -2,13 +2,16 @@ angular.module('video-player')
 
 .component('app', {
   templateUrl: 'src/templates/app.html',
-  controller: function() {
+  controller: function($scope) {
     this.videos = window.exampleVideoData;
     this.video = window.exampleVideoData[0];
+    $scope.vid = this.video;
     this.clickedOn = function(video) {
-      this.video = video;
-      console.log(this);
+      $scope.vid = video;
     };
+    $scope.$watch('vid', () => {
+      this.video=$scope.vid;
+    });
     // console.log(this);
   }
 });
--- src/components/videoListEntry.js
+++ src/components/videoListEntry.js
@@ -1,8 +1,8 @@
 angular.module('video-player')
 .component('videoListEntry', {
   templateUrl: 'src/templates/videoListEntry.html',
-  controller: function() {
-    // console.log(this);
+  controller: function($scope) {
+    console.log($scope.vid);
     this.somefunc = () => {
       //console.log(this.video);
       this.clickedOn(this.video);
