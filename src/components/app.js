3632d090b78131dd06cc898eae2525147e62e1e1
Benjamin Stephens <bs@NCC1776.local>
2017-09-11 21:36:08 -0700

    implements youtube api

--- src/components/app.js
+++ src/components/app.js
@@ -1,35 +1,40 @@
 angular.module('video-player')
 
-.component('app', {
-  templateUrl: 'src/templates/app.html',
-  controller: function($scope, $http) {
-    this.videos = window.exampleVideoData;
-    this.video = window.exampleVideoData[0];
-    $scope.vid = this.video;
-    this.clickedOn = function(video) {
-      $scope.vid = video;
-    };
-    $scope.$watch('vid', () => {
-      this.video=$scope.vid;
-    });
+  .component('app', {
+    templateUrl: 'src/templates/app.html',
+    controller: function($scope, $http) {
+      this.videos = window.exampleVideoData;
+      this.video = window.exampleVideoData[0];
+      $scope.vid = this.video;
+      $scope.vids = this.videos;
+      this.clickedOn = function(video) {
+        $scope.vid = video;
+      };
+      $scope.$watch('vid', () => {
+        this.video=$scope.vid;
+      });
+      $scope.$watch('vids', () => {
+        this.videos=$scope.vids;
+        this.video=$scope.vids[0];
+      });
 
-    $http({
-      method: 'GET',
-      url: 'https://www.googleapis.com/youtube/v3/search',
-      data: {
-        q: 'puppies',
-        part: 'snippet',
-        maxResults: 5,
-        key: window.YOUTUBE_API_KEY,
-        videoEmbeddable: true,
-        type: 'video',
-      },
-    }).then((data) => {
-      console.log(data);
-      $scope.vids = data.items;
-    }, () => {
-      console.log('hi');
-    });
-    console.log($http);
-  }
-});
+      $http({
+        method: 'GET',
+        url: 'https://www.googleapis.com/youtube/v3/search',
+        params: {
+          q: 'Kurzgesagt',
+          part: 'snippet',
+          maxResults: 5,
+          key: window.YOUTUBE_API_KEY,
+          videoEmbeddable: true,
+          type: 'video',
+        },
+      }).then((response) => {
+        console.log(response);
+        $scope.vids = response.data.items;
+      }, () => {
+        console.log('hi');
+      });
+      console.log($http);
+    }
+  });
